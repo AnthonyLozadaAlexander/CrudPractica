47df04931c98afda94e0e92e5f119b3d51977aaf
zDefcon <antholozadarequelme@gmail.com>
2026-02-19 04:37:13 -0500

    Agrega atributos de conexión a la clase Conexion
    Se añaden atributos para gestionar la conexión a la base de datos, incluyendo la URL, usuario y contraseña. Esto establece la base para futuras operaciones de conexión a la base de datos.

--- src/main/java/Conexion.java
+++ src/main/java/Conexion.java
@@ -1,2 +1,11 @@
-﻿public class Conexion {
+﻿import java.sql.Connection;
+
+public class Conexion {
+
+    Connection con = null;
+    String bd = "";
+    String url = "jdbc:mysql://localhost:3306/";
+    String user = "root";
+    String password = "";
+
 }
