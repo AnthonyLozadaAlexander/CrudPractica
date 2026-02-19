f923c06f2be12dc717b5ea311d820373df3ba85b
zDefcon <antholozadarequelme@gmail.com>
2026-02-19 04:48:22 -0500

    Agrega métodos para gestionar la conexión a la base de datos
    Se implementan los métodos getConexion y CloseConecction en la clase Conexion.
    El método getConexion establece la conexión a la base de datos y maneja errores
    con un mensaje de alerta. El método CloseConecction cierra la conexión y también
    notifica al usuario en caso de error.

--- src/main/java/Conexion.java
+++ src/main/java/Conexion.java
@@ -1,11 +1,40 @@
-﻿import java.sql.Connection;
+﻿import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Conexion {
 
     Connection con = null;
-    String bd = "";
+    String bd = "bd_usuarios";
     String url = "jdbc:mysql://localhost:3306/";
     String user = "root";
     String password = "";
 
+    public Connection getConexion(){
+
+        Connection con = null;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url);
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error: Database Failed to Connect" + e.getMessage());
+        }
+        return con;
+    }
+
+    public Connection CloseConecction(){
+        try{
+            if(con != null){
+                con.close();
+                JOptionPane.showMessageDialog(null, "Database Conection Closed");
+            }
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error: Database Failed to Connect" + e.getMessage());
+        }
+
+        return con;
+    }
 }
