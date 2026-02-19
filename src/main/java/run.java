3499c8ca27ea25e805222c529b2764c5a12a4407
zDefcon <antholozadarequelme@gmail.com>
2026-02-19 04:35:15 -0500

    Actualiza la interfaz de usuario en frmMain y establece el título de la ventana - Se agrega un fondo blanco a los componentes de la interfaz. - Se establece el título de la ventana principal como "Formulario De Registro".

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -3,8 +3,9 @@ import com.formdev.flatlaf.FlatLightLaf;
 public class run {
 
     public static void main(String[] args) {
-        frmMain window = new frmMain();
         FlatLightLaf.setup();
+        frmMain window = new frmMain();
+        window.setTitle("Formulario De Registro");
         window.setVisible(true);
         window.setLocationRelativeTo(null);
         window.setResizable(false);
